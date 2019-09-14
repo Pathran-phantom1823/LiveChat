093f95b0b11d5d1e67762a31b0a54b88da79e2ae
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2019-09-14 18:49:24 +0800

    adding is typing indicator

--- public/app.js
+++ public/app.js
@@ -3,30 +3,50 @@ const messageTypes = { LEFT: 'left', RIGHT: 'right', LOGIN: 'login' };
 //Chat stuff
 const chatWindow = document.getElementById('chat');
 const messagesList = document.getElementById('messagesList');
-const messageInput = document.getElementById('messageInput');
+var messageInput = document.getElementById('messageInput');
 const sendBtn = document.getElementById('sendBtn');
 var typeStatus = document.getElementById('typingalert');
 var typingDelayMillis = 5000;
 var lastTypedTime = new Date(0);
 
 //login stuff
-let username = '';
+var username = '';
 const usernameInput = document.getElementById('usernameInput');
 const loginBtn = document.getElementById('loginBtn');
 const loginWindow = document.getElementById('login');
+var inputM = document.getElementById('messageInput');
+var alert = document.getElementById('typingalert');
 
 const messages = []; // { author, date, content, type }
 
 //Connect to socket.io - automatically tries to connect on same port app was served from
 var socket = io();
 
-socket.on('message', function(message){
+socket.on('message', function (message) {
 	//Update type of message based on username
 	if (message.type !== messageTypes.LOGIN) {
 		if (message.author === username) {
 			message.type = messageTypes.RIGHT;
+			// document.addEventListener("click", function () {
+			// 	if (inputM.addEventListener("keydown", function () {
+			// 		alert.innerHTML = username + "Is Typing..."
+			// 	}));
+			// 	else if (inputM.addEventListener("keyup", function () {
+			// 		alert.innerHTML = ""
+			// 	}));
+
+			// });
 		} else {
 			message.type = messageTypes.LEFT;
+			// document.addEventListener("click", function () {
+			// 	if (inputM.addEventListener("keydown", function () {
+			// 		alert.innerHTML = username + "Is Typing..."
+			// 	}));
+			// 	else if (inputM.addEventListener("keyup", function () {
+			// 		alert.innerHTML = ""
+			// 	}));
+
+			// });
 		}
 	}
 
@@ -37,22 +57,22 @@ socket.on('message', function(message){
 	chatWindow.scrollTop = chatWindow.scrollHeight;
 });
 
-createMessageHTML = function(message){
+createMessageHTML = function (message) {
 	if (message.type === messageTypes.LOGIN) {
 		return `
 			<p class="secondary-text text-center mb-2">${
-				message.author
+			message.author
 			} joined the chat...</p>
 		`;
 	}
 	return `
 	<div class="message ${
 		message.type === messageTypes.LEFT ? 'message-left' : 'message-right'
-	}">
+		}">
 		<div class="message-details flex">
 			<p class="flex-grow-1 message-author">${
-				message.type === messageTypes.LEFT ? message.author : ''
-			}</p>
+		message.type === messageTypes.LEFT ? message.author : ''
+		}</p>
 			<p class="message-date">${message.date}</p>
 		</div>
 		<p class="message-content">${message.content}</p>
@@ -62,17 +82,23 @@ createMessageHTML = function(message){
 
 
 
-displayMessages = function(){
+displayMessages = function () {
 	const messagesHTML = messages
 		.map(message => createMessageHTML(message))
 		.join('');
 	messagesList.innerHTML = messagesHTML;
 };
 
+inputM.addEventListener("keypress", function () {
+	socket.emit("typing");
+})
+
+socket.on('typing', function (data) {
+	alert.innerHTML = "<p><i>" + data.username + " Is typing.." + "</i></p>";
+})
 
 
-
-sendBtn.addEventListener('click', function(e){
+sendBtn.addEventListener('click', function (e) {
 	e.preventDefault();
 	if (!messageInput.value) {
 		return console.log('Invalid input');
@@ -96,7 +122,8 @@ sendBtn.addEventListener('click', function(e){
 	messageInput.value = '';
 });
 
-loginBtn.addEventListener('click', function(e){
+
+loginBtn.addEventListener('click', function (e) {
 	e.preventDefault();
 	if (!usernameInput.value) {
 		return console.log('Must supply a username');
@@ -111,7 +138,7 @@ loginBtn.addEventListener('click', function(e){
 	chatWindow.classList.remove('hidden');
 });
 
-sendMessage = function(message){
+sendMessage = function (message) {
 	socket.emit('message', message);
 };
 
@@ -121,7 +148,7 @@ sendMessage = function(message){
 // 	}else{
 // 		typeStatus.innerHTML= username + "Is Typing";
 // 	}
-	
+
 // }
 
 // updatelastTime = function(){
--- server.js
+++ server.js
@@ -8,24 +8,37 @@ var port = process.env.PORT || 3000;
 //Serve public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, +'public/index.html'));
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 	console.log('a user connected');
 
-	socket.on('disconnect', function() {
+	socket.on('disconnect', function () {
 		console.log('user disconnected');
 	});
 
-	socket.on('message', function(message) {
+	socket.username = document.getElementById('usernameInput').value;
+
+	socket.on('message', function (message) {
 		console.log('message: ' + message);
 		//Broadcast the message to everyone
 		io.emit('message', message);
 	});
+
+	socket.on('typing', function(data){
+		console.log({author:io.username})
+		socket.broadcast.emit('typing', {username:socket.username})
+	})
+	
 });
 
-http.listen(port, function() {
+
+
+
+
+
+http.listen(port, function () {
 	console.log('listening on port 3000');
 });
